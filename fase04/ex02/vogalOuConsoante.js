9d99c5ebc9ee3bfc17f30da71e762fd47a8263d5
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2019-01-09 15:41:59 -0200

    feito ex02

---
+++ fase04/ex02/vogalOuConsoante.js
@@ -0,0 +1,7 @@
+function vogalOuConsoante(letra){
+    if (letra === "a" || letra === "e"|| letra === "i" || letra == "o" || letra === "u") {
+        return "Vogal";
+    } else {
+        return "Consoante";
+    }
+}
