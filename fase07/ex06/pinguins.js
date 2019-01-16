8067cccfdcb6a1b48d849f829c24dc1993477b68
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2019-01-16 11:07:19 -0200

    feito ex06

---
+++ fase07/ex06/pinguins.js
@@ -0,0 +1,10 @@
+function pinguins(arr) {
+    arr.map(e => {
+        e.roupa = {
+            cabeca: "oculos de sol",
+            camisa: "jaqueta polar",
+            pes: "tenis",
+        }
+    })
+    return arr;
+}
