69153972b57081b37c82a9f706f3cd94607a55b9
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2019-01-16 10:54:21 -0200

    feito ex04

---
+++ fase07/ex04/precoMedio.js
@@ -0,0 +1,14 @@
+function precoMedio(...args) {
+    let avg = 0;
+    let max = 0;
+    let maxName;
+    args[0].map((e, i) => {
+        avg += e.preco;
+        if (e.preco > max) {
+            max = e.preco;
+            maxName = e.nome;
+        }
+    });
+    console.log('O produto mais caro se chama "' + maxName + '"');
+    return avg / args[0].length;
+}
