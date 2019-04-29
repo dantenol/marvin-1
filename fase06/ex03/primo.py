e0b0cecdd1064257d76f0f333cea980ac17e68a1
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2019-04-29 08:22:03 -0300

    corrigido

--- fase06/ex03/primo.py
+++ fase06/ex03/primo.py
@@ -1,9 +1,9 @@
 def primo(n):
     if n < 2:
-        return False
+        return "Nao"
     i = 2
     while i < n:
         if n % i == 0:
-            return False
+            return "Nao"
         i += 1
-    return True
+    return "Sim"
