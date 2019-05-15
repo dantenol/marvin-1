5611a9bd03dd50631db71ddccfd1aa9b39daae15
dantenoalsco <44616776+dantenoalsco@users.noreply.github.com>
2019-05-15 19:11:06 -0300

    teste

--- fase06/ex04/somaPares.js
+++ fase06/ex04/somaPares.js
@@ -1,11 +1,3 @@
-function somaPares(n1, n2) {
-    let a = Math.ceil(n1);
-    let b = Math.floor(n2);
-    let sum = 0;
-    while (a <= b) {
-        if (a % 2 === 0)
-            sum += a;
-        a++;
-    }
-    return sum;
+function fibonacci(n) {
+    teste
 }
